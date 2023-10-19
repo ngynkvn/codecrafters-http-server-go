5b52e80dd34e5f5895d648167a1ead06da03adbb
Kevin Nguyen <knguyen189@bloomberg.net>
2023-10-19 16:01:10 -0400

    feat: pass stage 2 R/W

--- app/server.go
+++ app/server.go
@@ -13,9 +13,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+	buffer := make([]byte, 32768)
+	conn.Read(buffer)
+	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
 }
