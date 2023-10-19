8813f27edc922aa800f44b019a1a4e3c887bab69
Kevin Nguyen <knguyen189@bloomberg.net>
2023-10-19 16:11:06 -0400

    feat: pass stage 3

--- app/server.go
+++ app/server.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+type StartLine struct {
+	Method   string
+	Path     string
+	Protocol string
+}
+
+func NewStartLine(str string) StartLine {
+	words := strings.Split(str, " ")
+	return StartLine{
+		words[0],
+		words[1],
+		words[2],
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -20,6 +36,17 @@ func main() {
 	}
 	defer conn.Close()
 	buffer := make([]byte, 32768)
-	conn.Read(buffer)
-	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+	requestStr := string(buffer[:n])
+	lines := strings.Split(requestStr, "\r\n")
+	startLine := NewStartLine(lines[0])
+	if startLine.Path == "/" {
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		fmt.Fprintf(conn, "HTTP/1.1 404 NOT FOUND\r\n\r\n")
+	}
 }
